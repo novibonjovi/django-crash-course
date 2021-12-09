9a2412adefed3ca31e90377f36d1a972ead790d4
Novak Nikolic <novak.nikolic92@gmail.com>
2021-12-09 08:42:25 +0100

    Initial commit

---
+++ todo/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import redirect, render, redirect
+from django.http import HttpResponse
+from .models import Todo
+from .forms import TodoForm
+
+# Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all()
+    context = {
+        'todo_list': todos
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+# CRUD - Create, Retrieve, Update, Delete, List
+def todo_detail(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        'todo': todo
+    }
+    return render(request, 'todo/todo_detail.html', context)
+
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+        # create a todo object
+        pass
+    context = {'form': form}
+    return render(request, 'todo/todo_create.html', context)
+
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(request.POST or None, instance=todo)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    context = {'form': form}
+    return render(request, 'todo/todo_update.html', context)
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect('/')
